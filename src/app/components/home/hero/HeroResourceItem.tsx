18d59315a2f3ec99671b673a30018b48e91b52ad
oscarprdev <vlcoscar8@gmail.com>
2024-07-29 06:46:51 +0200

    Update translation text on hero images

--- src/app/components/home/hero/HeroResourceItem.tsx
+++ src/app/components/home/hero/HeroResourceItem.tsx
@@ -22,7 +22,7 @@ const HeroResourceItem = ({ resourceId, imgUrl, title }: HeroResourceItemProps) 
 						/>
 						<div className="absolute top-0 w-full h-full grid place-items-center opacity-0 group-hover:opacity-100 duration-500">
 							<div className="inset-0 w-full h-full absolute top-0 bg-black/50 blur-md"></div>
-							<p className="text-white uppercase text-lg font-black opacity-0 translate-y-2 group-hover:translate-y-0 group-hover:opacity-100 duration-700">
+							<p className="text-white uppercase text-lg font-black opacity-0 translate-y-10 group-hover:translate-y-0 group-hover:opacity-100 duration-500">
 								{title}
 							</p>
 						</div>
